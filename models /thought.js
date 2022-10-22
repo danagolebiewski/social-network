7d5b90d2304ff9e46aa88e5d8e0c4a4f352481b8
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-10-22 13:54:16 -0600

    updated thought.js model

--- models /thought.js
+++ models /thought.js
@@ -36,7 +36,7 @@ const thoughtSchema = new Schema(
     thoughtText: {
       type: String,
       required: true,
-      max_length: 280,
+      maxlength: 280,
       minlength: 1,
     },
     createdAt: {
